64bc8bd76ab7c27208f27beaa317cd96a02b8384
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2024-05-11 10:25:54 +0200

    css styling inline

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,11 +6,13 @@ const Counter = () => {
 
   const formatCount = () => (count === 0 ? <h1>Zero</h1> : count);
 
+  const styles = { color: "deeppink", border: "15px dashed green" };
+
   return (
     <>
       <img src={imageUrl} />
-      <span>{formatCount()}</span>
-      <div>hello world</div>
+      <span style={styles}>{formatCount()}</span>
+      <div style={{ color: "yellow" }}>hello world</div>
     </>
   );
 };
