f1f011629162b449936693d933d6dd679faf7c38
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2024-05-11 10:52:52 +0200

    rendering lists conditionally

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,27 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(6);
-  const [tagsList, setTagsList] = useState(["tag1", "tag2", "tag3"]);
+  const [tagsList, setTagsList] = useState([]);
 
   const formatCount = () => (count === 0 ? <h1>Zero</h1> : count);
 
   let classes = "badge m-2 badge-";
   classes += count === 0 ? "warning" : "primary";
 
-  return (
-    <>
+  const renderTags = () =>
+    tagsList.length !== 0 ? (
       <ul>
         {tagsList.map((tag) => (
           <li key={tag}>{tag}</li>
         ))}
       </ul>
+    ) : (
+      <div>There are no tags</div>
+    );
+
+  return (
+    <>
+      {renderTags()}
       <span className={classes}>{formatCount()}</span>
       <button className={"btn btn-secondary btn-sm"}>increment</button>
     </>
